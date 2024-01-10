c82efe72650fe377b2fd898fdcd836bef556c36e
ichi_being <dchisbvbph@gmail.com>
2024-01-10 14:49:30 +0900

    feat; 外部からlocalhost:3000にアクセスできるようにngrokを設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  #Blocked hostエラー回避のために追記
+  config.hosts << 'host.docker.internal'
+  config.hosts << '.ngrok-free.app'
 end
