302ca3f7803a6d835d1fb241999c914b03bf3864
ichi_being <dchisbvbph@gmail.com>
2024-01-12 00:53:03 +0900

    feat: User controller作成とログイン関係のヘルパーメソッドを作成する

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # 静的ページ
   get '/terms_of_service', to: 'static_pages#terms_of_service'
   get '/privacy_policy', to: 'static_pages#privacy_policy'
+
+  resource :user, only: %i[new create]
 end
