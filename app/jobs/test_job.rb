4334e3227cbadc4d15a8ba55619cf8fe3db480d3
ichi_being <dchisbvbph@gmail.com>
2024-01-28 10:56:46 +0900

    feat: docker環境でLINE PUSH通知をSidekiqで送れるかテストする

--- app/jobs/test_job.rb
+++
@@ -1,9 +0,0 @@
-class TestJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    puts "===================="
-    puts "Test job is performed at #{Time.now}"
-    puts "===================="
-  end
-end
