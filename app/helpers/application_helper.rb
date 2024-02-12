4a5d628115dc568f013fbcb22ad02c9ae4b6b6c4
ichi_being <dchisbvbph@gmail.com>
2024-02-12 23:44:20 +0900

    feat: meta-tagsの設定ファイル作成,view,helperにコード追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,33 @@ module ApplicationHelper
     base_title = 'Floss Buddy'
     title.present? ? "#{title} | #{base_title}" : base_title
   end
+
+  def default_meta_tags
+    {
+      site: 'Floss Buddy',
+      title: 'LINEで手軽に。フロスの習慣化！',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'LINEでフロスの習慣化をサポートするアプリです。',
+      keywords: 'フロス,デンタルフロス,オーラルケア',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('OGP.png'),
+        local: 'ja-JP'
+      },
+      # Twitter用の設定を個別で設定する
+      twitter: {
+        card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
+        site: '@ichi_being', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
+        image: image_url('OGP.png') # 配置するパスやファイル名によって変更すること
+      }
+    }
+  end
+
 end
